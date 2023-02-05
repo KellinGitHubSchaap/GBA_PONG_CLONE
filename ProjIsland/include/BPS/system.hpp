2ecffa64ef021690fa4e3b2a5c7f78e7d4175ab6
Kellin Schaap <78432932+KellinGitHubSchaap@users.noreply.github.com>
2023-02-05 11:25:16 +0100

    Delete ProjIsland directory

--- ProjIsland/include/BPS/system.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SYSTEM_HPP
-#define SYSTEM_HPP
-
-#include "../include/BPS/types.hpp"
-#include "../include/BPS/gba.hpp"
-
-INLINE void VBlankIntrWait() { SystemCall(5); }
-
-#endif // SYSTEM_HPP
